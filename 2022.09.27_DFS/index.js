9baaf17eef39cafd46c24fc55e6ff249f0f4977b
김선자 <seonjakim@gimseonjas-MacBook-Pro.local>
2022-09-29 06:01:03 +0900

    dfs

--- 2022.09.27_DFS/index.js
+++ 2022.09.27_DFS/index.js
@@ -33,3 +33,61 @@ function solution(n) {
   DFS(v)
   return answer
 }
+
+// 합이 같은 부분집합
+function solution(arr) {
+  let answer = 'no', flag = 0
+  let total = arr.reduce((a, b) => a + b, 0)
+  const len = arr.length
+  function DFS(L, sum) {
+    if (flag) return
+    if (L === len) {
+      if ((total - sum) === sum) { 
+        answer = "yes"
+        flag = 1
+      }
+      return
+    }
+    DFS(L + 1, sum + arr[L])
+    DFS(L + 1, sum)
+  }
+  DFS(0, 0)
+  return answer
+}
+
+// 이진트리 DFS
+function solution(c, arr) {
+  let answer = Number.MIN_SAFE_INTEGER
+  const len = arr.length
+  function DFS(L, sum) {
+    if (sum > c) return
+    if (L === len) {
+      answer = Math.max(answer, sum)
+    } else {
+      DFS(L + 1, sum + arr[L])
+      DFS(L + 1, sum)
+    }
+  }
+  DFS(0, 0)
+  return answer
+}
+
+const arr = [81, 58, 42, 33, 61]
+
+// 최대점수 구하기 (이진트리 DFS)
+const pv = [10, 25, 15, 6, 7]
+const pt = [5, 12, 8, 3, 4]
+function solution(m, pv, pt) {
+  let answer
+  const len = pv.length
+  function DFS(L, sum, t) {
+    if (t > m) return
+    if (L > len) {
+      answer = Math.max(answer, sum)
+      return
+    }
+    DFS(L + 1, )
+  }
+  DFS(0, 0, 0)
+}
+
