955a54ce6eed013d6051d9a4e9ae913866a2ac69
김선자 <seonja.kim0@gmail.com>
2022-10-01 21:56:59 +0900

    union number, sequence

--- 2022.09.27_DFS/index.js
+++ 2022.09.27_DFS/index.js
@@ -160,3 +160,87 @@ function solution(n) {
   }
   return DFS(n)
 }
+
+// 조합수
+function solution(n, r) {
+  let answer
+  let dy = Array.from(Array(n), () => Array(r).fill(0))
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r]
+    if (n === r || r === 0) return 1
+    return dy[n][r] = DFS(n -1, r -1) + DFS(n - 1, r)
+  }
+  return answer
+}
+
+// 수열 추측하기
+function solution(n, f) {
+  let answer, flag = 0
+  const dy = Array.from(Array(n), () => Array(n).fill(0))
+  const ch = Array.from({length : n + 1}, () => 0)
+  const p = Array.from({length : n}, () => 0)
+  const b = Array.from({length : n}, () => 0)
+
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r]
+    if (n === r || r === 0) return 1
+    return dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r)
+  }
+  function DFS(L, sum) {
+    if (flag) return
+    if (L === n && sum === f) {
+      answer = p.slice()
+      flag = 1
+      return
+    }
+    for (let i = 1; i <= n; i++) {
+      if (ch[i] === 0) {
+        ch[i] = 1
+        p[L] = i
+        DFS(L + 1, sum + (b[L] * p[L]))
+        ch[i] = 0
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i)
+  }
+  DFS(0, 0)
+  return answer
+}
+// 수열 추측하기 => push pop 사용해서
+function solution(n, f) {
+  let answer, flag = 0
+  const dy = Array.from(Array(n), () => Array(n).fill(0))
+  const ch = Array.from({length : n + 1}, () => 0)
+  const p = []
+  const b = Array.from({length : n}, () => 0)
+
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r]
+    if (n === r || r === 0) return 1
+    return dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r)
+  }
+  function DFS(L, sum) {
+    if (flag) return
+    if (L === n && sum === f) {
+      answer = p.slice()
+      flag = 1
+      return
+    }
+    for (let i = 1; i <= n; i++) {
+      if (ch[i] === 0) {
+        ch[i] = 1
+        p.push(i)
+        DFS(L + 1, sum + (b[L] * p[L]))
+        ch[i] = 0
+        p.pop()
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i)
+  }
+  DFS(0, 0)
+  return answer
+}
