1878e903f9f9281ff988542c21345d13b84f8ac4
김선자 <seonja.kim0@gmail.com>
2022-10-06 22:36:55 +0900

    bfs

--- 그래프와 탐색/index.js
+++ 그래프와 탐색/index.js
@@ -82,3 +82,40 @@ function solution(board) {
   board[0][0] = 1
   DFS(0, 0)
 }
+
+// 송아지 찾기(BFS)
+function solution(s, e) {
+  let ch = Array.from({length: 10001}, () => 0)
+  let dis = Array.from({length: 10001}, () => 0)
+  let queue = []
+  ch[s] = 1
+  queue.push(s)
+  dis[s] = 0
+  while (queue.length) {
+    let x = queue.shift()
+    for (let nx of [x - 1, x + 1, x + 5]) {
+      if (nx === e) return dis[x] + 1
+      if (nx > 0 && nx <= 10000 && ch[nx] === 0) {
+        ch[nx] = 1
+        queue.push(nx)
+        dis[nx] = dis[x] + 1
+      }
+    }
+  }
+}
+
+// 왜 안될까??
+function solution(s, e) {
+  const set = new Set()
+  const dis = [-1, 1, 5]
+  function BFS(L, cur) {
+    if (cur === e) return L
+    for (let i = 0; i < dis.length; i++) {
+      const nx = cur + dis[i]
+      if (set.has(nx)) continue
+      set.add(nx)
+      BFS(L + 1, nx)
+    }
+  }
+  return BFS(0, s)
+}
