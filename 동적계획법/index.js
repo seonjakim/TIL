e4ab804fadc4cc6aeab987d7b1a95c4b75008cd7
김선자 <seonja.kim0@gmail.com>
2022-10-24 23:11:35 +0900

    dp

--- 동적계획법/index.js
+++ 동적계획법/index.js
@@ -1,25 +1,25 @@
-// 계단오르기
-// function solution(n) {
-//   let dy = Array.from({ length: n + 1 }, () => 0);
-//   dy[1] = 1;
-//   dy[2] = 2;
-//   for (let i = 3; i <= n; i++) {
-//     dy[i] = dy[i - 2] + dy[i - 1];
-//   }
-//   return dy[n];
-// }
+// 계단오르기;
+function solution(n) {
+  let dy = Array.from({ length: n + 1 }, () => 0);
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dy[i] = dy[i - 2] + dy[i - 1];
+  }
+  return dy[n];
+}
 
-// 최대부분증가수열(LIS)
-// function solution(arr) {
-//   let answer = 0;
-//   let dy = Array.from({ length: arr.length }, () => 0);
-//   dy[0] = 1;
-//   for (let i = 1; i < arr.length; i++) {
-//     const cur = arr.slice(i - 1);
-//   }
+// 최대부분증가수열(LIS);
+function solution(arr) {
+  let answer = 0;
+  let dy = Array.from({ length: arr.length }, () => 0);
+  dy[0] = 1;
+  for (let i = 1; i < arr.length; i++) {
+    const cur = arr.slice(i - 1);
+  }
 
-//   return answer;
-// }
+  return answer;
+}
 
 function solution(arr) {
   let dy = Array.from({ length: arr.length + 1 }, () => 0);
@@ -49,3 +49,23 @@ function solution(m, coin) {
   }
   return dy[m];
 }
+
+// 최대점수 구하기(냅색 알고리즘)
+function solution(t, p) {
+  let dy = Array.from({ length: t + 1 }, () => 0);
+  for (let i = 0; i < p.length; i++) {
+    const [score, time] = p[i];
+    for (let j = t; j >= time; j--) {
+      dy[j] = Math.max(dy[j - time] + score, dy[j]);
+    }
+  }
+  return dy[t];
+}
+
+solution(20, [
+  [10, 5],
+  [25, 12],
+  [15, 8],
+  [6, 3],
+  [7, 4],
+]);
