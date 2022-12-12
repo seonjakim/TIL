9fc6529d526ef822fac7c56d423046dbef89bc7f
seonja <seonja.kim0@gmail.com>
2022-12-12 23:53:28 +0900

    implement max binary heap

--- 2022.12.12/BinaryHeap.js
+++ 2022.12.12/BinaryHeap.js
@@ -12,22 +12,61 @@ class MaxBinaryHeap {
   bubbleUp() {
     let idx = this.values.length - 1
     const element = this.values[idx]
-    let flag
+    let swap
     do {
-      flag = false
+      swap = false
       let parentIdx = Math.floor((idx - 1) / 2)
       let parent = this.values[parentIdx]
       if (parent && parent < element) {
         this.values[parentIdx] = element
         this.values[idx] = parent
         idx = parentIdx
-        flag = true
+        swap = true
       }
-    } while (flag)
+    } while (swap)
+  }
+  remove() {
+    if (this.values.length <= 1) {
+      this.values.pop()
+    } else {
+      let i = 0
+      this.values[i] = this.values.pop()
+      let swap
+      do {
+        swap = false
+        let current = this.values[i]
+        let left = i * 2 + 1
+        let leftVal = this.values[left]
+        let right = i * 2 + 2
+        let rightVal = this.values[right]
+        if (current < leftVal || current < rightVal) {
+          swap = true
+          if (leftVal > rightVal) {
+            this.values[left] = current
+            this.values[i] = leftVal
+            i = left
+          } else if (rightVal > leftVal) {
+            this.values[right] = current
+            this.values[i] = rightVal
+            i = right
+          } else swap = false
+        }
+      } while (swap)
+    }
+    console.log(this.values)
   }
 }
 
 const heap = new MaxBinaryHeap()
-heap.insert(30)
+// heap.insert(30)
 heap.insert(55)
-heap.insert(45)
+// heap.insert(45)
+heap.remove()
+heap.remove()
+heap.remove()
+heap.remove()
+heap.remove()
+heap.remove()
+heap.remove()
+heap.remove()
+heap.remove()
