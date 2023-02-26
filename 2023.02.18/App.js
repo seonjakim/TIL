30d534844f0f18e8195bc9e01eb81bc6ad744ea6
seonjakim <seonja.kim0@gmail.com>
2023-02-26 18:56:10 +0900

    implement SearchInput onChange event and Suggestion component

--- 2023.02.18/App.js
+++ 2023.02.18/App.js
@@ -1,10 +1,45 @@
+import { fetchLanguages } from "./api"
+import SearchInput from "./SearchInput"
+import Suggestion from "./Suggestion"
+
 export default function App({ $target }) {
     this.state = {
         fetchedLanguages: [],
         selectedLanguages: []
     }
 
-    this.setState = (nextState) => {
+    const searchInput = new SearchInput({
+      $target,
+      initialState: '',
+      onChange: async (keyword) => {        
+        if (keyword.length === 0) {
+          this.setState({
+            fetchedLanguages: []
+          })
+        } else {
+          const languages = await fetchLanguages(keyword)
+          this.setState({
+            fetchedLanguages: languages
+          })
+        }
+      }
+    })
 
+    const suggestion = new Suggestion({
+      $target,
+      initialState: {
+        items: []
+      }
+    })
+
+    this.setState = (nextState) => {
+      this.state = {
+        ...this.state,
+        ...nextState
+      }
+
+      suggestion.setState({
+        items: this.state.fetchedLanguages
+      })
     }
 }
